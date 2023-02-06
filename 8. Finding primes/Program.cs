6c75308d73fc35c8360d6afa3467fcd0f77caf4b
Adam Neumann <57113336+bulaimaslo@users.noreply.github.com>
2023-02-07 00:52:03 +0100

    Added solution 8.Finding primes

---
+++ 8. Finding primes/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _8._Finding_primes;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int T = int.Parse(Console.ReadLine());
+        for (int i = 0; i < T; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int m = int.Parse(input[0]);
+            int n = int.Parse(input[1]);
+            Console.WriteLine(FindPrimes(m, n));
+        }
+    }
+
+    static int FindPrimes(int m, int n)
+    {
+        int count = 0;
+        for (int i = m; i <= n; i++)
+        {
+            if (IsPrime(i))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    static bool IsPrime(int n)
+    {
+        if (n < 2)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+}
