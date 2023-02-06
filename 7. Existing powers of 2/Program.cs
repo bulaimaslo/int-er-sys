8ff3b06fb7f30205c0083def3c82a3ba11f42b8d
Adam Neumann <57113336+bulaimaslo@users.noreply.github.com>
2023-02-07 00:40:36 +0100

    Added solution 7.Existing powers of 2

---
+++ 7. Existing powers of 2/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace _7._Existing_powers_of_2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<uint> input = new List<uint>();
+
+        while (true)
+        {
+            string line = Console.ReadLine();
+            if (string.IsNullOrEmpty(line))
+                break;
+
+            input.Add(uint.Parse(line));
+        }
+
+        List<uint> powers = PowersOfTwo(input);
+        powers.Sort();
+        System.Console.WriteLine(string.Join(" ", powers));
+
+    }
+
+    static List<uint> PowersOfTwo(List<uint> input)
+    {
+        HashSet<uint> powerNumbers = new HashSet<uint>();
+
+        foreach (var number in input)
+        {
+            if (number == 0)
+            {
+                continue;
+            }
+            else if (number == 1)
+            {
+                powerNumbers.Add(number);
+            }
+            else
+            {
+                HashSet<uint> decomposed = DecomposeNumber(number);
+                powerNumbers.UnionWith(decomposed);
+            }
+        }
+
+        return powerNumbers.ToList();
+    }
+
+    static HashSet<uint> DecomposeNumber(uint number)
+    {
+        HashSet<uint> powers = new HashSet<uint>();
+
+        while (number > 1)
+        {
+            uint power = 1;
+            while (power * 2 <= number)
+            {
+                power *= 2;
+            }
+
+            powers.Add(power);
+            number -= power;
+        }
+
+        return powers;
+    }
+
+
+}
