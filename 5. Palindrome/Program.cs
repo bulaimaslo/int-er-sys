c3f54c5596c05473c36915869c0aa41a9b53f90b
Adam Neumann <57113336+bulaimaslo@users.noreply.github.com>
2023-02-06 23:52:13 +0100

    Added 5.Palindrome solution

---
+++ 5. Palindrome/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _5._Palindrome;
+using System.Text;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string inputOnlyLetters = RemoveNonLetters(input);
+        System.Console.WriteLine(IsPalindrome(inputOnlyLetters) ? "yes" : "no");
+    }
+
+    static bool IsPalindrome(string input)
+    {
+        for (int i = 0; i < input.Length / 2; i++)
+        {
+            if (input[i] != input[input.Length - 1 - i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static string RemoveNonLetters(string input)
+    {
+        StringBuilder result = new StringBuilder();
+        foreach (char c in input)
+        {
+            if (char.IsLetter(c))
+            {
+                result.Append(c);
+            }
+        }
+
+        return result.ToString();
+    }
+}
